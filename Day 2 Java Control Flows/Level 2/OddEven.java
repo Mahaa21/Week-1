b9915d01c04238aafc0aa2753334029d9ca0caa4
Mahaa21 <mahalakshmibabu2106@gmail.com>
2025-03-19 18:00:00 +0530

    Add OddEven.java

---
+++ Day 2 Java Control Flows/Level 2/OddEven.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class OddEven {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = scanner.nextInt();
+        if (num > 1) {
+            for (int i = 1; i <= num; i++) {
+                if (i % 2 == 0) {
+                    System.out.println(i + " is Even");
+                } else {
+                    System.out.println(i + " is Odd");
+                }
+            }
+        } else {
+            System.out.println("Number is not a Natural Number");
+        }
+    }
+}
