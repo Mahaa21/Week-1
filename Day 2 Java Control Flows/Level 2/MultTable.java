e1b31ea124ee14313128738d1672d5f64f69667e
Mahaa21 <mahalakshmibabu2106@gmail.com>
2025-03-19 18:00:00 +0530

    Add MultTable.java

---
+++ Day 2 Java Control Flows/Level 2/MultTable.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class MultTable {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        System.out.println("\nMultiplication Table of " + number + " from 6 to 9:");
+        for (int i = 6; i <= 9; i++) {
+            System.out.println(number + " * " + i + " = " + (number * i));
+        }
+    }
+}
